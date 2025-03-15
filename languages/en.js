20ea4d070fe5777a3e38e138b49390dc67b5f0a4
Vtubejeje <143926810+Vtubejeje@users.noreply.github.com>
2025-03-15 16:17:07 +0530

    en.js

--- languages/en.js
+++ languages/en.js
@@ -255,7 +255,7 @@ module.exports = {
   support: {
     embed: {
         authorName: "Support Server",
-        description: "➡️ **Join our Discord server for support and updates:**\n- Discord - {supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - {githubLink}\n- Replit - {replitLink}\n- YouTube - {youtubeLink}",
+        description: "➡️ **Join our Discord server for support and updates:**\n- Discord - {supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - {githubLink}\n- YouTube - {youtubeLink}",
         error: "Error",
         errorDescription: "- An error occurred while processing your request."
     }
