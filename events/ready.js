d97376f0ea5de1be219feab2249163e3f5ae3b58
Vtubejeje <143926810+Vtubejeje@users.noreply.github.com>
2025-03-15 15:57:09 +0530

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -43,7 +43,7 @@ module.exports = async (client) => {
         //console.log(`🎵 Now Playing: ${trackName}`);
 
         client.user.setActivity({
-            name: `🎸 ${trackName}`,
+            name: `✨ ${trackName}`,
             type: ActivityType.Playing
         });
     }
